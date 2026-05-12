700f5cd6251a2eca6a43971fefbe6810244a0770
Tia2710 <tiaaam278@gmail.com>
2026-05-13 01:13:35 +0700

    Delete KEDAI TIA - FOOD AND DRINK directory

--- KEDAI TIA - FOOD AND DRINK/js/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle Class Aktif untuk search form
-const searchForm = document.querySelector(".search-form");
-const searchBox = document.querySelector("#search-box");
-
-document.querySelector("#search-button").onclick = () => {
-  searchForm.classList.toggle("active");
-};
